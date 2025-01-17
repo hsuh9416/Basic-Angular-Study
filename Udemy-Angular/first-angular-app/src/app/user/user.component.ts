e630bbd06fa901c4ad946c89a53ac3ae13f54821
hsuh7 <hsuh7@hawk.iit.edu>
2025-01-17 16:37:35 -0600

    Added signal function

--- Udemy-Angular/first-angular-app/src/app/user/user.component.ts
+++ Udemy-Angular/first-angular-app/src/app/user/user.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 //ng generate component user
 @Component({
@@ -10,13 +12,21 @@ import { DUMMY_USERS } from '../../dummy-users';
 })
 export class UserComponent {
   // state that can be changed -> zone.js
-  selectedUser = DUMMY_USERS[Math.floor(Math.random()* DUMMY_USERS.length)];
+  // selectedUser = DUMMY_USERS[randomIndex];
 
-  get imagePath(){ // getter
-    return 'assets/users/' + this.selectedUser.avatar
-  }
+  // Using signal
+  selectedUser = signal(DUMMY_USERS[randomIndex])
+
+  // get imagePath(){ // getter
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
+  
+  // Using computed with signal
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
   onSelectUser(){ // event - random profile change
-    this.selectedUser = DUMMY_USERS[Math.floor(Math.random()* DUMMY_USERS.length)];
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
